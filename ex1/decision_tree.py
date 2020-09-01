293eeb8e25193745a3d1b843629e84428001149c
AdilsonAngelo <adilson.angelo.jr@gmail.com>
2020-09-01 18:19:50 -0300

    code cleanup

--- ex1/decision_tree.py
+++ ex1/decision_tree.py
@@ -4,6 +4,8 @@ from anytree import Node, RenderTree
 
 class DecisionTreeClassifier:
     def _col_gini(self, x: pd.Series, y: pd.Series):
+        '''calcula grau de impureza da coluna x'''
+
         res = dict()
         for v in pd.unique(x):
             pos = len(y[y & (x == v)])
@@ -14,29 +16,36 @@ class DecisionTreeClassifier:
         return sum(v * len(x[x == k]) / len(x) for k, v in res.items())
 
     def _gini(self, X: pd.DataFrame, y: pd.Series):
+        '''gera tuplas do formato: (column, gini_index)'''
         for col in X:
             yield (col, self._col_gini(X[col], y))
 
-    def _fit(self, X: pd.DataFrame, y: pd.Series, parent_col='root', parent_val=''):    # noqa
+    def _gen_tree(self, X: pd.DataFrame, y: pd.Series, parent_col='root', parent_val=''):   # noqa
+        '''retorna o no raiz da arvore de decisao'''
+        # se todos forem da mesma classe retorna nó folha
         if y.all() or (~y).all():
-            return Node(parent_col, val=parent_val, target=y.all())                     # noqa
+            return Node(parent_col, val=parent_val, target=y.all())                         # noqa
 
-        this_col, _ = sorted([g for g in self._gini(X, y)], key=lambda tup: tup[1])[0]       # noqa
+        # pega coluna com menor entropia
+        this_col, _ = sorted([g for g in self._gini(X, y)], key=lambda tup: tup[1])[0]      # noqa
 
-        children = []
-        for val in pd.unique(X[this_col]):
-            child = self._fit(X[X[this_col] == val],
-                              y[X[this_col] == val], this_col, val)
-            children.append(child)
+        children = [
+            self._gen_tree(X[X[this_col] == val],
+                           y[X[this_col] == val], this_col, val)
+
+            for val in pd.unique(X[this_col])
+        ]
 
         return Node(parent_col, children=children, val=parent_val, target='')
 
     def fit(self, dataset: pd.DataFrame, target: pd.Series):
-        self.tree = RenderTree(self._fit(dataset, target))
+        '''constroi a arvore de decisao'''
+        self.tree = RenderTree(self._gen_tree(dataset, target))
 
     def rules(self):
+        '''retorna uma string com a representacao da arvore de decisao'''
         if self.tree is not None:
 
             # print(self.tree)
-            for pre, fill, node in self.tree:
-                print(f'{pre}{node.name}({node.val}): {node.target}')
+            return '\n'.join(f'{pre}{node.name}({node.val}): {node.target}'
+                             for pre, _, node in self.tree)
--- ex1/main.py
+++ ex1/main.py
@@ -1,13 +1,18 @@
+import os
 import pandas as pd
 from decision_tree import DecisionTreeClassifier
 
 
 def main():
-    df = pd.read_csv('./data/play.csv', index_col='Dia')
+    curr_dir = os.path.dirname(__file__)
+    csv_file = os.path.join(curr_dir, 'data/play.csv')
+
+    df = pd.read_csv(csv_file, index_col='Dia')
     X, y = df.loc[:, df.columns != 'Jogar'], df['Jogar']
+
     clf = DecisionTreeClassifier()
     clf.fit(X, y)
-    clf.rules()
+    print(clf.rules())
 
 
 if __name__ == "__main__":
